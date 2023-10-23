45e106869a58b83bae3f780dc866ddc9eaf00347
mariozhou <mariozhou@tencent.com>
2023-10-23 14:11:11 +0800

    1

---
+++ mergeTxt.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob')
+const Epub = require("epub-gen");
+
+const main = async () => {
+  console.log("main start")
+
+  var output = "./锵锵三人行.txt"
+  
+  if(fs.existsSync(output)) {
+    fs.unlinkSync(output)
+  }
+
+  const files = glob.sync("./**/*.txt")
+  
+ files.sort()
+
+  console.log("files count: " + files.length)
+  var content = ""
+
+  var epubContents = []
+
+  files.forEach((file)=>{
+    var name = path.parse(file).name
+    var str = fs.readFileSync(file,'utf8')
+
+    var dst = []
+    var lines = str.split('\n');
+    lines = lines.splice(1);
+    lines.forEach(line=>{
+      var isMatch = /说话人 \d .*/g.exec(line)
+      if(!isMatch) {
+        dst.push(line)
+      }
+    })
+
+    var str = dst.join('\n')
+    str = "## " + name + "\n" + str;
+
+    var epubData = ""
+    dst.forEach(line=>{
+      epubData += `<p>${line}</p>`
+    })
+
+    content += str;
+
+    epubContents.push({title:name,author:"",data:epubData})
+  })
+
+
+  fs.writeFileSync(output,content);
+
+  const option = {
+    title: "锵锵三人行",
+    author: "", 
+    publisher: "",
+    cover: "", // Url or File path, both ok.
+    content: epubContents
+};
+
+  new Epub(option, "./锵锵三人行.epub");
+
+
+  console.log("main end")
+
+};
+
+main().catch((err) => console.error(err));
